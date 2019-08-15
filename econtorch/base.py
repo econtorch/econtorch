88e58677c6b5f775edde0e3501ac9079597e5b02
Mario Milone <milonemario@gmail.com>
2019-08-14 18:59:39 -0700

    update model

--- econtorch/base.py
+++ econtorch/base.py
@@ -4,19 +4,43 @@ import torch
 from abc import *   # For abstract classes 
 
 class _Agent(ABC):
+    r"""
+    Class _Agent provide the base class for all the dynamic problems
+    We note:
+    - pi(a|s) the current policy
+    - v_pi(s) the value of being in state s given pi
+    - q_pi(s,a) the value of taking action a in state s given pi
+    """ 
     
     def __init__(self):
         pass
 
     @abstractmethod
-    def value(state, action):
+    def policy(state):
+        r"""
+        Return the current policy given the state
+        pi(a|s) = probability that At=a is St=s
+        """
         pass
 
     @abstractmethod
-    def policy(state):
+    def value(state):
+        r"""
+        Return the expected value of a state under the current policy
+        v_pi(s)
+        """
         pass
-
+    
     @abstractmethod
     def reward(state, action):
         pass
+
+    def action_value(state, action):
+        r"""
+        Return the expected value of taking action a
+        in state s under the current policy
+        q_pi(s,a)
+        """
+        pass
+
 
--- models/OptimalFinancing.py
+++ models/OptimalFinancing.py
@@ -4,7 +4,10 @@ Based on "Dynamic Models and Structural Estimation in Corporate Finance"
 by Streabulaev and Whited (2012)
 """
 
+import torch
+import numpy as np
 from econtorch.base import _Agent
+
 
 
 class Agent(_Agent):
@@ -22,39 +25,56 @@ class Agent(_Agent):
          sigma]     Noise of the AR1 shock process
 
     """
+    
 
     # Methods that need implementing for the class _Agent
-    def __init__(self, theta, delta, r, rho, sigma):
+    def __init__(self):
+        # Global parameters
+        nk = 100        # Grid size for capital
+        k_min = 0
+        k_max = 100
+        nz = 10         # Grid size for the productivity shock
+        z_min = 0
+        z_max = 10
+        # Model parameters
+        self.theta = 0.7
+        self.delta = 0.15
+        self.r = 0.04
+        self.phi0 = 0.01
+        self.rho = 0.7
+        self.sigma = 0.15
+        # State space
+        k = np.linspace(k_min, k_max, nk)
+        z = np.linspace(z_min, z_max, nz)
+        km, zm = np.meshgrid(k, z, indexing='ij')
+        self.states = torch.tensor([km, zm])
+
+
+    def reward(self, states, actions):
+        # Cash flow
+        return _cashFlow(states[0], actions[0], states[1])
+        
+    def value(self, states, actions):
         pass
 
-    def reward(state, action):
-        # Cash Flow function
-        
-    def 
+    def policy(self, states):
+        pass
+
+    def action_value(state, action):
+        pass
 
 
     # Methods specific to this problem
     # Profit function
-    def _prof(state,params):
-        k=state['k']
-        z=state['z']
-        theta=params['theta']
-        return z*(k**theta)
+    def _prof(self, k, z):
+        return z*(k**self.theta)
     
     # Adjustment Costs
-    def _adjCosts(state,controls,params):
-        k=state['k']
-        I=controls['I']
-        phi0=params['phi0']
-        phi1=params['phi1']
-        fixedCosts=0
-        if I!=0:
-            fixedCosts=phi1*k
-        return phi0*(I**2)/(2*k)+fixedCosts
+    def _adjCosts(self, I, k):
+        return self.phi0*(I**2)/(2*k)
     
     # Cash Flow function
-    def _cashFlow(state,controls,params):
-        I=controls['I']
-        return prof(state,params)-I-adjCosts(state,controls,params)
+    def _cashFlow(self, k, I, z):
+        return _prof(k,z)-I-_adjCosts(I,k)
 
 
