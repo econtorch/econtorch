607c67dd3ee2709eb8c6f90f081546c533e7580c
Mario Milone <milonemario@gmail.com>
2020-01-17 11:27:53 -0800

    Update doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../econtorch'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- For ReadTheDocs --------------------------------------------------------
 master_doc = 'index'
